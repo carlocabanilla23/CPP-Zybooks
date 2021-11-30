f87ca7bdaf83c82cd87d83e059f45cfea150c37d
John Carlo Cabanilla <80723157+carlocabanilla23@users.noreply.github.com>
2021-11-30 09:45:45 -0800

    Update Fibonnaci
    Removed the comments and whitespaces;

--- Fibonnaci/main.cpp
+++ Fibonnaci/main.cpp
@@ -2,61 +2,27 @@
 using namespace std;
 
 int Fibonacci(int n) {
-      
-      int x =0;
-      int y= 1;
-     
-        if (n%2==1){
-              for(int i =0;i<(n-1)/2;i++){
-         
-         
-             x= x+y;  //     1,3,8
-            
-            
-            
-             y=y+x;  //  2,5,13
-           
-      
-             
-             
-         
-         }
-         
-        n=y;
-  
-            
-        }
-////////////////////////////////////////        
+      int x = 0;
+      int y = 1;
+        if ( n%2 == 1 ){
+             for( int i = 0 ; i<(n-1)/2 ; i++ ){
+                  x = x + y;  
+                   y = y + x;  
+         }  
+            n = y; 
+        }       
          else{
-              for(int i =0;i<n/2;i++){
-         
-         
-             x= x+y;  //     1,3,8
-            
-            
-            
-             y=y+x;  //  2,5,13
-           
-      
-             
-             
-         
+             for( int i = 0 ; i<n/2 ; i++ ){
+                  x = x + y; 
+                  y = y + x; 
          }
-         
-        n=x;
-  
-            
+            n = x;  
         }
-    
-      return n;
-   
+            return n;
 }
-
 int main() {
    int startNum;
-      
    cin >> startNum;
    cout << "Fibonacci(" << startNum << ") is " << Fibonacci(startNum) << endl;
-
    return 0;
 }
